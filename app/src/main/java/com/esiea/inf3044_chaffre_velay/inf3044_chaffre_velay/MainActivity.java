03a0f37253a8130efbe8ab9c34dd5cc8eeb4fbe3
Marc-Velay <marc.velay@gmail.com>
2016-12-09 16:48:21 +0100

    initial commit

---
+++ app/src/main/java/com/esiea/inf3044_chaffre_velay/inf3044_chaffre_velay/MainActivity.java
@@ -0,0 +1,13 @@
+package com.esiea.inf3044_chaffre_velay.inf3044_chaffre_velay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
