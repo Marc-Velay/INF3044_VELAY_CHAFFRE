2dc5cc8f537138528095db2bfbdb18cda2ce1e27
Marc-Velay <marc.velay@gmail.com>
2016-12-19 11:52:08 +0100

    corrected a spelling mistake and menus

--- app/src/main/java/com/esiea/inf3044_chaffre_velay/inf3044_chaffre_velay/MovieSearchActivity.java
+++ app/src/main/java/com/esiea/inf3044_chaffre_velay/inf3044_chaffre_velay/MovieSearchActivity.java
@@ -1,5 +1,7 @@
 package com.esiea.inf3044_chaffre_velay.inf3044_chaffre_velay;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -21,7 +23,7 @@ public class MovieSearchActivity extends AppCompatActivity {
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        getMenuInflater().inflate(R.menu.search_menu, menu);
         return true;
     }
 
@@ -29,6 +31,9 @@ public class MovieSearchActivity extends AppCompatActivity {
         int id = item.getItemId();
         if (id == R.id.search_history) {
             Toast.makeText(getApplicationContext(), getString(R.string.search_history), Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.menu_main) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
         }
         return super.onOptionsItemSelected(item);
     }
